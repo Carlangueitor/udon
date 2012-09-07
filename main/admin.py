a0a031261c2b37c0ccff0d519ab26fe91d4a2828
Charly Román <chack14rock@gmail.com>
2012-09-07 15:00:18 -0500

    Added models: UserProfile, Review, CheckIn

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from main.models import Show, Demography, Genre, Episode
+from main.models import Show, Demography, Genre, Episode, Movie, LiveAction, Ova
 
 admin.site.register(Demography)
 admin.site.register(Genre)
 admin.site.register(Show)
 admin.site.register(Episode)
+admin.site.register(Movie)
+admin.site.register(LiveAction)
+admin.site.register(Ova)
--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Demography(models.Model):
     name = models.CharField(max_length=20)
@@ -14,7 +15,7 @@ class Genre(models.Model):
     image = models.ImageField(upload_to='images/')
     
     def __unicode__(self):
-        return self.name
+        return self.name  
     
 class Anime(models.Model):
     title = models.CharField(max_length=60)
@@ -25,18 +26,22 @@ class Anime(models.Model):
     synopsis = models.TextField()
     page = models.URLField()
     related = models.ManyToManyField('self')
+    rated =  models.BooleanField()
     rate = models.DecimalField(max_digits=4, decimal_places=2)
     
     def __unicode__(self):
         return self.title
     
-    class Meta:
-        abstract = True
+#    class Meta:
+#        abstract = True
         
 class Episode(models.Model):
     title = models.CharField(max_length=50)
     number = models.IntegerField()
     image = models.ImageField(upload_to='images/')
+    
+    def __unicode__(self):
+        return self.title
             
 class Show(Anime):
     STATUS_CHOICES = (
@@ -63,3 +68,26 @@ class Bagde(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
+    
+    def __unicode__(self):
+        return self.name
+
+class CheckIn(models.Model):
+    anime = models.ForeignKey(Anime)
+    points = models.IntegerField()
+    comment = models.CharField(max_length=100)  
+      
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    title = models.CharField(max_length=50)
+    chekins = models.ManyToManyField(CheckIn)
+    bagdes = models.ManyToManyField(Bagde)
+    points = models.IntegerField()      
+
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    rate = models.DecimalField(max_digits=3, decimal_places=2)
+    coment = models.TextField()
+    anime = models.ForeignKey(Anime)
+    user = models.ForeignKey(User)
+    date = models.DateField()
